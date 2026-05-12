0d6358f2e874f8e14f92f27325920ce36aeaf96b
Aminulhoque01 <aminuhoque53@gmail.com>
2026-05-12 22:19:47 +0600

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,4 @@
-// import "./globals.css";
-// import ProviderWrapper from "../redux/ProviderWrapper";
 
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body suppressHydrationWarning>
-//         <ProviderWrapper>{children}</ProviderWrapper>
-//       </body>
-//     </html>
-//   );
-// }
 
 
 
